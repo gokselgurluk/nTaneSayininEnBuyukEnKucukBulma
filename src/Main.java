281cb24ade4d7146da4a91f58f22a78591a6e66f
göksel gürlük <gurlukgoksel@gmail.com>
2024-02-22 13:33:45 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main (String[] args){
+
+         int number=0,numInput ,minNumber=0,maxNumber=0,type;
+
+         Scanner userInp = new Scanner(System.in);
+
+         System.out.println("Kaç tane sayı gireceksiniz ?");
+         numInput= userInp.nextInt();
+
+         for(int i = 1 ; i <= numInput ;i++){
+
+             System.out.println("Sayıyı giriniz: ");
+             number= userInp.nextInt();
+             maxNumber=Math.max(maxNumber,number);
+             minNumber=Math.min(minNumber,number);
+         }
+
+
+         System.out.println("En büyük sayi: "+maxNumber);
+         System.out.println("En küçük sayi: "+minNumber);
+
+
+     }
+}
